1bb8ea0ffb4a63c3fd6d3bd875643cfc93fb9017
Christian Berndt <christian.berndt@liferay.com>
2024-09-01 17:06:53 +0200

    chore: initial check-in.

---
+++ src/main/java/com/chberndt/springbootacl/SpringBootAclApplication.java
@@ -0,0 +1,13 @@
+package com.chberndt.springbootacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAclApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAclApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chberndt/springbootacl/SpringBootAclApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chberndt.springbootacl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAclApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
