706411473b7d52fd7dd98966298d74f6be300b00
Christian Berndt <berndt@kulturtechniker.ch>
2024-12-20 00:27:03 +0100

    chore: enable test users with USER and ADMIN role.

--- src/main/java/com/chberndt/springbootacl/config/SecurityConfiguration.java
+++ src/main/java/com/chberndt/springbootacl/config/SecurityConfiguration.java
@@ -3,12 +3,36 @@ package com.chberndt.springbootacl.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Christian Berndt
  */
 @Configuration
 public class SecurityConfiguration {
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsService() {
+
+		UserDetails admin = User.withUsername("admin")
+			.password(passwordEncoder().encode("test"))
+			.roles("ADMIN")
+			.build();
+
+		UserDetails user = User.withUsername("user").password(passwordEncoder().encode("test")).roles("USER").build();
+
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
