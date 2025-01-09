1e48c57b7b73bfe86b659f87b21a5d4a2f24c21b
Christian Berndt <christian.berndt@liferay.com>
2025-01-09 23:55:00 +0100

    fix: make sure the transaction advisor runs before the authorization advisor.

--- src/main/java/com/chberndt/springbootacl/config/SecurityConfiguration.java
+++ src/main/java/com/chberndt/springbootacl/config/SecurityConfiguration.java
@@ -14,11 +14,13 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author Christian Berndt
  */
 @Configuration
+@EnableTransactionManagement(order = 0)
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
